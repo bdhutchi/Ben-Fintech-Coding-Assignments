fa4bdfd1b16fcd6751b8956f213cab10be988ba3
bdhutchi <62345751+bdhutchi@users.noreply.github.com>
2020-03-18 11:31:51 -0400

    Update Fintech BSM Option Pricing function (1).py

--- Fintech BSM Option Pricing function (1).py
+++ Fintech BSM Option Pricing function (1).py
@@ -19,18 +19,18 @@ def BSMOptionPrice(option,S,K,T,r,vol):
 # In[13]:
 
 
-d1 = (np.log(S / K) + (r + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
-d2 = (np.log(S / K) + (r - 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    d1 = (np.log(S / K) + (r + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
 
 
 # In[10]:
 
 
-if option == 'call':
+    if option == 'call':
     result = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
-elif option == 'put':
+    elif option == 'put':
     result = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
-return result
+    return result
 
 
 # In[14]:

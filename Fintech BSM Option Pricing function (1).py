84ca5262eaff7fa51d8d8dd62baf0bac4109a89c
bdhutchi <62345751+bdhutchi@users.noreply.github.com>
2020-03-18 11:24:45 -0400

    Add files via upload

---
+++ Fintech BSM Option Pricing function (1).py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+def BSMOptionPrice(option,S,K,T,r,vol):
+    #S = Spot Price
+    #K = Strike Price
+    #T = Time to Maturity
+    #r = Risk Free Rate
+    #Vol= Asset Volatility
+
+    import numpy as np
+    import scipy.stats as si
+    
+
+
+# In[13]:
+
+
+d1 = (np.log(S / K) + (r + 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+d2 = (np.log(S / K) + (r - 0.5 * vol ** 2) * T) / (vol * np.sqrt(T))
+
+
+# In[10]:
+
+
+if option == 'call':
+    result = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+elif option == 'put':
+    result = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+return result
+
+
+# In[14]:
+
+
+print(BSMOptionPrice('call',100.0,75.0,0.5,.0125,.35))
+
+
+# In[ ]:
+
+
+
+
---
+++ Minimum Payment Function.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Minimum Payment:
+
+def f(balance,payment,monthlyInterestRate,term):
+     newbalance = balance
+     for month in range(1, term+1):
+        newbalance -= payment
+        newbalance += monthlyInterestRate * newbalance
+print (month, newbalance)
+print ("Final balance"+newbalance)
+     return (newbalance)
+
+
+balance=320000
+annualInterestRate=0.2
+term=12
+epsilon=0.01
+
+
+
+def find_minimum_payment(balance, annualInterestRate, term, epsilon):
+n = 1
+low=balance / term
+monthlyInterestRate=annualInterestRate / 12
+high=(balance * (1 + monthlyInterestRate)**12) / term
+maxIterations=1000
+while n <= maxIterations: # limit iterations to prevent infinite loop
+guess = (low + high)/2 # new midpoint
+residual=f(balance,guess,monthlyInterestRate,term)
+if abs(residual) <= epsilon:
+print(round(guess,2))
+break
+elif residual > 0:
+low=guess
+else:
+high=guess
+n = n + 1 # increment step counter
+return(guess)
+
+balance=320000
+annualInterestRate=0.2
+term=12
+epsilon=0.01
+
+print(find_minimum_payment(balance,annualInterestRate,term,epsilon)
+#print(balance)
+
